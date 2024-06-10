41807e4859898e63a9d5f5237b9c6edb7668609e
Jonathan Chen <jonathanchen1122@gmail.com>
2024-06-10 16:29:37 -0700

    VR Hand grab (in progress)

---
+++ VR-Simulator/Assets/Scripts/FirstPersonCam.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonCam : MonoBehaviour
+{
+    public float sensitivityX, sensitivityY;
+    [SerializeField] float yRotation, xRotation;
+    public Transform playerOrientation;
+
+    void Start()
+    {
+        // cursor set to middle and invisible
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        // tracking input mouse movements
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensitivityX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensitivityY;
+
+        // increment rotation with respect mouse inputs via Quaternion method
+        yRotation += mouseX;
+        xRotation -= mouseY;
+
+        // clamping to limit the vertical view range between -90 and 90
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        // set the values to the rotation
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+
+        // rotate the player horizontally
+        if (playerOrientation)
+        {
+            playerOrientation.rotation = Quaternion.Euler(0, yRotation, 0);
+        }
+    }
+}
---
+++ VR-Simulator/Assets/Scripts/HandGrabDesktopMode.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class HandGrabDesktopMode : MonoBehaviour
+{
+    [SerializeField] Animator leftHandAnim;
+    [SerializeField] Animator rightHandAnim;
+
+    string actionType = "Grip";
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.J))
+        {
+            if (leftHandAnim.GetFloat(actionType) == 1)
+            {
+                leftHandAnim.SetFloat(actionType, 0);
+            }
+
+            else
+            {
+                leftHandAnim.SetFloat(actionType, 1);
+            }
+        }
+
+        else {
+            if (leftHandAnim.GetFloat(actionType) > 0)
+            {
+                leftHandAnim.SetFloat(actionType, leftHandAnim.GetFloat(actionType)-1);
+            }
+        }
+
+
+        if (Input.GetKey(KeyCode.K))
+        {
+            if (rightHandAnim.GetFloat(actionType) == 1)
+            {
+                rightHandAnim.SetFloat(actionType, 0);
+            }
+
+            else
+            {
+                rightHandAnim.SetFloat(actionType, 1);
+            }
+        }
+
+    }
+}
